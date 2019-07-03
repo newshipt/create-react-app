a2ba022c265e962fb770330906093c03c1a6782c
Hugh Boylan <hugh@shipt.com>
2019-07-03 15:35:20 -0500

    update template with nova context changes

--- packages/react-scripts/template-typescript/src/context.ts
+++ packages/react-scripts/template-typescript/src/context.ts
@@ -1,44 +1,30 @@
-import { ContextExporter, ISG1ModuleContext, IMenuItem, ISG1Route } from '@shipt/nova/context'
+import { ContextExporter, ISG1ModuleContext, IMenuSection } from '@shipt/nova/context'
 
 const id = require('../package.json').name
 
 export class Context implements ISG1ModuleContext {
-  ID = id
-  get Menu(): IMenuItem[] {
-    let routes: ISG1Route[] = [];
-
-    let sampleHomeRoute = {
-      path: "/sample",
-      component: 'SampleDefault',
-      exact: true
-    };
-    routes.push(sampleHomeRoute);
-
-    let sampleHome: IMenuItem = {
-      Routes: routes,
-      Display: "Sample Home",
-      Permissions: []
-    };
-
-    let sampleEditRoute = {
-      path: "/samples/edit",
-      component: 'SampleEdit'
-    };
-    routes = [];
-    routes.push(sampleEditRoute);
-
-    let sampleEdit = {
-      Routes: routes,
-      Display: "Create a sample",
-      Permissions: []
-    };
-
-    let menuItems: IMenuItem[] = [];
-    menuItems.push(sampleHome);
-    menuItems.push(sampleEdit);
-
-    return menuItems;
-  }
+    ID = id
+    get Menu(): IMenuSection[] {
+        return [{
+            sort: 0,
+            name: 'Sample',
+            items: [{
+                sort: 0,
+                name: 'Sample Home',
+                routes: [{
+                    path: '/sample',
+                    component: 'SampleDefault'
+                }]
+            }, {
+                sort: 1,
+                name: 'Sample Edit',
+                routes: [{
+                    path: '/sample/edit',
+                    component: 'SampleEdit'
+                }]
+            }]
+        }]
+    }
 }
 
 ContextExporter.export(new Context())

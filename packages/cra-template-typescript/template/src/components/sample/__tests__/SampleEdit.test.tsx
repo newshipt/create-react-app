935a1dfe97ede0e3902cda35362b543e39385801
Hugh Boylan <hugh@shipt.com>
2019-12-10 10:42:51 -0600

    fix tests and update deps

--- packages/cra-template-typescript/template/src/components/sample/__tests__/SampleEdit.test.tsx
+++ packages/cra-template-typescript/template/src/components/sample/__tests__/SampleEdit.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { render } from '@testing-library/jest-dom'
+import { MemoryRouter as Router } from 'react-router-dom'
+import { render } from '@testing-library/react'
 import { SampleEdit } from '../SampleEdit'
+
+const getWrapper = (): React.FC => ({ children }) => (
+  <Router>{children}</Router>
+)
 
 describe('SampleEdit', () => {
   it('should render without errors', () => {
-    const component = render(<SampleEdit />)
+    const wrapper = getWrapper()
+    const component = render(<SampleEdit />, { wrapper })
     return expect(component.findByText('Sample Edit')).toBeDefined()
   })
 })
--- packages/cra-template-typescript/template/src/components/sample/__tests__/SampleHome.test.tsx
+++ packages/cra-template-typescript/template/src/components/sample/__tests__/SampleHome.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { render } from '@testing-library/jest-dom'
+import { MemoryRouter as Router } from 'react-router-dom'
+import { render } from '@testing-library/react'
 import { SampleHome } from '../SampleHome'
+
+const getWrapper = (): React.FC => ({ children }) => (
+  <Router>{children}</Router>
+)
 
 describe('SampleHome', () => {
   it('should render without errors', () => {
-    const component = render(<SampleHome />)
+    const wrapper = getWrapper()
+    const component = render(<SampleHome />, { wrapper })
     return expect(component.findByText('Sample Home')).toBeDefined()
   })
 })
